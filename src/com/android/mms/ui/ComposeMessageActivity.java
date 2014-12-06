a6b8ffac1166e5e76b06ec894eb222303a84c9e4
Matt Garnes <matt@cyngn.com>
2014-12-05 17:57:02 -0800

    Add support for sharing vCalendar events over MMS.
    Support any ACTION_SEND intent that contains a file stream extra with
    the mimetype text/x-vcalendar. This Intent can will open the
    ComposeMessageActivity with a Calendar event attached.
    
    Change-Id: I33816ceb4feebfa657f3659d5ef0d721803a056f

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -546,6 +546,7 @@ public class ComposeMessageActivity extends Activity
                 case AttachmentEditor.MSG_PLAY_VIDEO:
                 case AttachmentEditor.MSG_PLAY_AUDIO:
                 case AttachmentEditor.MSG_PLAY_SLIDESHOW:
+                case AttachmentEditor.MSG_VIEW_VCAL:
                 case AttachmentEditor.MSG_VIEW_VCARD:
                     if (mWorkingMessage.getSlideshow() != null) {
                          viewMmsMessageAttachment(msg.what);
@@ -554,6 +555,7 @@ public class ComposeMessageActivity extends Activity
                 case AttachmentEditor.MSG_REPLACE_IMAGE:
                 case AttachmentEditor.MSG_REPLACE_VIDEO:
                 case AttachmentEditor.MSG_REPLACE_AUDIO:
+                case AttachmentEditor.MSG_REPLACE_VCAL:
                 case AttachmentEditor.MSG_REPLACE_VCARD:
                     showAddAttachmentDialog(true);
                     break;
@@ -625,6 +627,7 @@ public class ComposeMessageActivity extends Activity
                             case WorkingMessage.VIDEO:
                             case WorkingMessage.AUDIO:
                             case WorkingMessage.VCARD:
+                            case WorkingMessage.VCAL:
                             case WorkingMessage.SLIDESHOW:
                                 MessageUtils.viewMmsMessageAttachment(ComposeMessageActivity.this,
                                         msgItem.mMessageUri, msgItem.mSlideshow,
@@ -2494,6 +2497,7 @@ public class ComposeMessageActivity extends Activity
 
         mIsPickingContact = false;
         addRecipientsListeners();
+
         if (isRecipientsEditorVisible()) {
             mRecipientsEditor.addTextChangedListener(mRecipientsWatcher);
         }
@@ -3884,6 +3888,11 @@ public class ComposeMessageActivity extends Activity
         handleAddAttachmentError(result, R.string.type_vcard);
     }
 
+    private void addVCal(Uri uri) {
+        int result = mWorkingMessage.setAttachment(WorkingMessage.VCAL, uri, false);
+        handleAddAttachmentError(result, R.string.type_vcal);
+    }
+
     AsyncDialog getAsyncDialog() {
         if (mAsyncDialog == null) {
             mAsyncDialog = new AsyncDialog(this);
@@ -3999,6 +4008,9 @@ public class ComposeMessageActivity extends Activity
                     && (type.equals("text/x-vcard")
                     || (wildcard && isVcardFile(uri)))) {
                 addVcard(uri);
+           } else if ((type.equals("text/x-vcalendar")
+                       && isVCalFile(uri))) {
+                addVCal(uri);
            }
         }
     }
@@ -5365,6 +5377,13 @@ public class ComposeMessageActivity extends Activity
     private boolean isVcardFile(Uri uri) {
         String path = uri.getPath();
         return null != path && path.toLowerCase().endsWith(".vcf");
+    }
+
+    // Get the path of uri and compare it to ".vcs" to judge whether it is a
+    // vcalendar file.
+    private boolean isVCalFile(Uri uri) {
+        String path = uri.getPath();
+        return null != path && path.toLowerCase().endsWith(".vcs");
     }
 
     private ListView getListView() {
